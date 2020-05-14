0a83a4d030bc176068f47e056e758f0087046b4c
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-14 13:28:35 +1000

    Worked through CallbackFunctions

--- CallbackFunctions.js
+++ CallbackFunctions.js
@@ -1,11 +1,28 @@
 let people = [
     {
         name: "Luke",
-        message: "Hi"
+        message: "Hi",
+        heightInches: 73
     },
     {
         name: "Dan",
-        message: "I'm Dan!"
+        message: "I'm Dan!",
+        heightInches: 69
+    },
+    {
+        name: "Stephanie",
+        message: "Hi",
+        heightInches: 62
+    },
+    {
+        name: "Gerry",
+        message: "Hey",
+        heightInches: 64
+    },
+    {
+        name: "David",
+        message: "Hello",
+        heightInches: 60
     }
 ];
 
@@ -44,9 +61,49 @@ function myFilterFunction(person) {
     }
 }
 
-let filteredArray = people.filter(myFilterFunction);
+let filteredArray = people.filter(function (person) {
+    if (person.name == "Luke") {
+        return true; // will be included in filtered array
+    } else {
+        return false; // will be excluded from filtered array
+    }
+});
 
-console.log("people");
+// console.log("people");
+// console.log(people);
+// console.log("filteredArray");
+// console.log(filteredArray);
+
+
+function getNames(person){
+    return person.name;
+}
+
+function convertHeightToCm(person){
+    return {
+        name: person.name,
+        message: person.message,
+        heightInCm: person.heightInches * 2.54
+    };
+}
+
+let peopleCm = people.map(convertHeightToCm); // outputs ['Luke','Dan']
+
 console.log(people);
-console.log("filteredArray");
-console.log(filteredArray);
+console.log(peopleCm);
+
+
+let newArr = [];
+for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+    newArr.push(person.name);
+}
+
+// Array.find
+
+function findUnder65(person) {
+    return person.heightInches < 65;
+}
+
+let person = people.find(findUnder65);
+console.log(person);
